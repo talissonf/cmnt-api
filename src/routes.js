9365a44bf545fa71a60dea164c15f7c199aa3302
Talisson Ferreira <talissonf@gmail.com>
2018-11-25 17:32:14 -0200

    Add: new routes

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,10 @@
 const router = require('express').Router()
 const validateToken = require('./services/token')
 const users = require('./resources/users')
+const blogs = require('./resources/blogs')
+const articles = require('./resources/articles')
+const comments = require('./resources/comments')
+
 // função next para debug rotas
 router.use((request, response, next) => {
   console.log('%s em : api%s', request.method, request.path) //debug
@@ -19,5 +23,38 @@ router.route('/users/:id')
   .get(validateToken, users.get)
   .put(validateToken, users.put)
   .delete(validateToken, users.delete)
+
+/**
+ * Blogs
+ */
+router.route('/blogs')
+  .post(validateToken, blogs.post)
+
+router.route('/blogs/:id')
+  .get(validateToken, blogs.get)
+  .put(validateToken, blogs.put)
+  .delete(validateToken, blogs.delete)
+/**
+ * Articles
+ */
+router.route('/articles')
+  .post(validateToken, articles.post)
+
+router.route('/articles/:id')
+  .post(validateToken, articles.post)
+  .get(validateToken, articles.get)
+  .put(validateToken, articles.put)
+  .delete(validateToken, articles.delete)
+/**
+ * Comments
+ */
+router.route('/comments')
+  .post(validateToken, comments.post)
+
+router.route('/comments/:id')
+  .post(validateToken, comments.post)
+  .get(validateToken, comments.get)
+  .put(validateToken, comments.put)
+  .delete(validateToken, comments.delete)
 
 module.exports = router
