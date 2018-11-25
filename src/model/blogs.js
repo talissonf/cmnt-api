302b4374d4ef1d15c5fe65e276c22a8abc9df405
Talisson Ferreira <talissonf@gmail.com>
2018-11-25 12:35:08 -0200

    Blogs schema

---
+++ src/model/blogs.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose')
+const MongoSchema = mongoose.Schema
+
+let schema = {
+  created_at: {
+    type: Date,
+    default: Date.now
+  },
+  uid: {
+    type: String,
+    required: 'userid field is required.'
+  },
+  title: {
+    type: String,
+    required: 'blog title is riquired.'
+  },
+  count: {
+    comments: {
+      type: Number
+    },
+    articles: {
+      type: Number
+    },
+    followes: {
+      type: Number
+    },
+    views: {
+      total: {
+        type: Number
+      },
+      unique: {
+        type: Number
+      }
+    }
+  },
+  pictures: {
+    small: {
+      type: String
+    },
+    large: {
+      type: String
+    }
+  },
+  slug: {
+    type: String,
+    required: false
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now
+  }
+}
+
+let modelSchema = new MongoSchema(schema)
+module.exports = mongoose.model('blogs', modelSchema)
